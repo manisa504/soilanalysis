bbcc779444d2eb8165b01f2fafbe58ed243139b9
Manisha Shah <97705567+manisa504@users.noreply.github.com>
2024-04-20 08:52:58 -0700

    Update app.py

--- app.py
+++ app.py
@@ -3,14 +3,6 @@
 
 import streamlit as st
 import pandas as pd
-numpy==1.20.1
-streamlit==0.76.0
-seaborn==0.11.1
-matplotlib==3.4.1
-plotly==4.14.3
-pandas==1.2.3
-joblib==0.16.0
-scikit-learn
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn import metrics
